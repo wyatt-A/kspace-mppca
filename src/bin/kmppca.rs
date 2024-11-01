b9dfcd96b04900e3f1ca305abfec2c06055ba187
wyatt-A <wa41@duke.edu>
2024-11-01 17:31:18 -0400

    update

--- src/bin/kmppca.rs
+++ src/bin/kmppca.rs
@@ -1,12 +1,162 @@
-use recon2::{config::config::TomlConfig, preprocessor::preprocessor::Preprocessor};
+use core::ops::Range;
+use std::path::Path;
+use cfl::{CflReader, CflWriter};
+use cfl::{ndarray::ShapeBuilder, num_complex::Complex32};
+use cfl::ndarray::{Array1, Array2};
+use kspace_mppca::{extract_data, insert_data, singular_value_threshold_mppca};
+use mr_data::kspace::KSpace;
+use recon2::{config::config::TomlConfig, object_manager::object_manager::ObjectManager};
 
 fn main() {
-    println!("kmppca");
+
+    let work_dir = "/privateShares/wa41/denoise_test2";
+
+    let object_manager = ObjectManager::from_file("/privateShares/wa41/24.chdi.01.work/240725-12-1/S69963/object-data/object-manager").unwrap();
+    let total_objs = object_manager.total_objects().unwrap();
+
+    println!("total objects: {}",total_objs);
+
+    // extract view coordinates
+    let k = object_manager.kspace(0).expect("failed to get kspace 0");
+
+    if k.len() > 0 {
+        println!("warning! Only using first kspace object!");
+    }
+
+    let phase_encoding_coords = k[0].coords();
+    let n_views = phase_encoding_coords.len();
+    let nx = k[0].line_len();
+    println!("nx: {}",nx);
+    println!("n views: {}",n_views);
+
+    let norm = |kx:i32,ky:i32,kz:i32| -> i32 {
+        kx*kx + ky*ky + kz*kz
+    };
+
+    let mut sample_ordering = vec![];
+    let mut idx = 0;
+    phase_encoding_coords.iter().for_each(|phase_encode|{
+        for kx in k_range(nx) {
+            let l2 = norm(kx,phase_encode[0],phase_encode[1]);
+            sample_ordering.push(
+                SampleOrdering {linear_idx:idx,norm:l2}
+            );
+            idx += 1;
+        }
+    });
+    let n_samples_per_vol = idx;
+    println!("n_samples_per_vol: {}",n_samples_per_vol);
+    let chunk_size = 1000;
+
+    sample_ordering.sort_by_key(|order|order.norm);
+    
+    let mut flattened = vec![];
+    let mut denoised = vec![];
+    let mut outputs = vec![];
+
+
+    // encode
+    for i in 1..11 {
+        println!("{}",i);
+        let ksp = object_manager.kspace(i).unwrap().remove(0);
+        //let ksp = KSpace::from_file(format!("k{:02}",i)).unwrap();
+        let c = ksp.to_array2(&phase_encoding_coords).into_dyn();
+        let mut sorted = Array1::<Complex32>::from_elem(c.len().f(), Complex32::ZERO);
+        let c_slice = c.as_slice_memory_order().unwrap();
+        sorted.iter_mut().zip(sample_ordering.iter()).for_each(|(elem,order)|{
+            //println!("r.0 = {}",r.0);
+            *elem = c_slice[order.linear_idx]
+        });
+
+        let p = Path::new(work_dir).join(civm_rust_utils::num_label(i,total_objs,"r"));
+        let d = Path::new(work_dir).join(civm_rust_utils::num_label(i,total_objs,"d"));
+        let o = Path::new(work_dir).join(civm_rust_utils::num_label(i,total_objs,"k"));
+
+        cfl::from_array(
+            &p,
+            &sorted.into_dyn()
+        ).unwrap();
+
+        flattened.push(p);
+        denoised.push(d);
+        outputs.push(o);
+
+    }
+
+    // denoise
+    // let skip_indices = [0, 11, 22, 33, 44, 55]; // Indices to skip
+
+    // let filtered_range: Vec<usize> = (0..67)
+    //     .filter(|x| !skip_indices.contains(x)) // Filter out the unwanted indices
+    //     .collect();
+
+    let readers:Vec<_> = flattened.iter().map(|file|{
+        CflReader::new(file).unwrap()
+    }).collect();
+
+    let mut writers:Vec<_> = denoised.iter().map(|file|{
+        CflWriter::new(file,&[n_samples_per_vol]).unwrap()
+    }).collect();
+ 
+    //let mut init_energy = CflWriter::new("init_energy",&[16200]).unwrap();
+    //let mut final_energy = CflWriter::new("final_energy",&[16200]).unwrap();
+    //let mut singular_values = CflWriter::new("singular_values",&[61,16200]).unwrap();
+
+    //let mut idx:Vec<usize> = (0..590).collect();
+    //let mut sv_idx:Vec<usize> = (0..61).collect();
+ 
+    let indices:Vec<usize> = (0..n_samples_per_vol).collect();
+
+    for (i,chunk) in indices.chunks(chunk_size).enumerate() {
+        println!("working on {}",i);
+        let mut c_mat = extract_data(&chunk, &readers);
+        //println!("shape: {:?}",c_mat.shape());
+        let _ = singular_value_threshold_mppca(&mut c_mat, None);
+
+        insert_data(&chunk, &mut writers, &c_mat);
+        //idx.iter_mut().for_each(|x| *x += 590);
+        //sv_idx.iter_mut().for_each(|x| *x += 61);
+    }
+ 
+    // println!("flushing writers ...");
+    // for w in writers {
+    //     w.flush().expect("failed to flush writer");
+    // }
+
+    for (output,den) in outputs.iter().zip(denoised.iter()) {
+        println!("reconstructing {}",den.display());
+        let denoised = cfl::to_array(den, true).unwrap();
+        let mut compressed = Array2::from_elem((nx,n_views).f(), Complex32::ZERO);
+        let c = compressed.as_slice_memory_order_mut().unwrap();
+        denoised.iter().zip(sample_ordering.iter()).for_each(|(x,r)|{
+            c[r.linear_idx] = *x;
+        });
+        let ksp = KSpace::from_array2(compressed, &phase_encoding_coords);
+        ksp.write_to_file(output).unwrap();
+    }
+
+    // decode
+
+}
+
+fn k_range(n:usize) -> Range<i32> {
+
+    assert!(n!=0,"n must be greater than 0");
+
+    if n%2 == 0 {
+        -((n/2) as i32) .. ((n/2) as i32)
+    }else {
+        -((n/2) as i32) .. ceiling_div(n, 2) as i32
+    }
+}
+
+pub fn ceiling_div(a:usize,b:usize) -> usize {
+    (a + b - 1) / b
 }
 
 
-#[test]
-fn prep() {
-
-
+struct SampleOrdering {
+    linear_idx:usize,
+    // value of the norm that is used to rank the samples
+    norm:i32,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::btree_map::RangeMut;
+
 use cfl::{ndarray::{parallel::prelude::*, Array2, Axis, ShapeBuilder}, num_complex::Complex32, CflReader, CflWriter};
 use ndarray_linalg::SVDDC;
 
@@ -11,71 +13,71 @@ pub struct DenoiseInfo {
 
 pub fn singular_value_threshold_mppca(matrix:&mut Array2<Complex32>, rank:Option<usize>) -> DenoiseInfo {
 
-        let m = matrix.shape()[0];
-        let n = matrix.shape()[1];
+    let m = matrix.shape()[0];
+    let n = matrix.shape()[1];
 
-        let init_energy = matrix.iter().map(|x|x.norm_sqr()).sum::<f32>();
+    let init_energy = matrix.iter().map(|x|x.norm_sqr()).sum::<f32>();
 
-        //let mut _s = Array2::from_elem((m,n), Complex32::ZERO);
+    //let mut _s = Array2::from_elem((m,n), Complex32::ZERO);
 
-        let nn = m.min(n);
-        let mut _s = Array2::from_elem((nn,nn), Complex32::ZERO);
+    let nn = m.min(n);
+    let mut _s = Array2::from_elem((nn,nn), Complex32::ZERO);
 
-        //let (_,s,_) = matrix.svd(true, true).unwrap();
-        // let (_,sigma_sq) = marchenko_pastur_singular_value(&s.as_slice().unwrap(), m, n);
-        // matrix.par_mapv_inplace(|x| x / sigma_sq);
-        //let (u,mut s,v) = matrix.svd(true, true).unwrap();
+    //let (_,s,_) = matrix.svd(true, true).unwrap();
+    // let (_,sigma_sq) = marchenko_pastur_singular_value(&s.as_slice().unwrap(), m, n);
+    // matrix.par_mapv_inplace(|x| x / sigma_sq);
+    //let (u,mut s,v) = matrix.svd(true, true).unwrap();
 
-        let (u,mut s,v) = matrix.svddc(ndarray_linalg::UVTFlag::Some).unwrap();
+    let (u,mut s,v) = matrix.svddc(ndarray_linalg::UVTFlag::Some).unwrap();
 
-        let u = u.unwrap();
-        let v = v.unwrap();
+    let u = u.unwrap();
+    let v = v.unwrap();
 
-        // println!("u:{:?}",u.shape());
-        // println!("s:{:?}",s.shape());
-        // println!("v:{:?}",v.shape());
+    // println!("u:{:?}",u.shape());
+    // println!("s:{:?}",s.shape());
+    // println!("v:{:?}",v.shape());
 
-        let mut noise = None;
+    let mut noise = None;
 
-        let rank = rank.unwrap_or_else(||{
-            let (rank,sigma_sq) = marchenko_pastur_singular_value(&s.as_slice().unwrap(), m, n);
-            noise = Some(sigma_sq);
-            rank
-        });
-        
-        let singular_values:Vec<_> = s.iter().map(|&s|Complex32::new(s, 0.)).collect();
+    let rank = rank.unwrap_or_else(||{
+        let (rank,sigma_sq) = marchenko_pastur_singular_value(&s.as_slice().unwrap(), m, n);
+        noise = Some(sigma_sq);
+        rank
+    });
+    
+    let singular_values:Vec<_> = s.iter().map(|&s|Complex32::new(s, 0.)).collect();
 
-        s.iter_mut().enumerate().for_each(|(i,val)| if i >= rank {*val = 0.});
+    s.iter_mut().enumerate().for_each(|(i,val)| if i >= rank {*val = 0.});
 
-        //let u = u.unwrap();
-        //let v = v.unwrap();
-        let mut diag_view = _s.diag_mut();
-        diag_view.assign(
-            &s.map(|x|Complex32::new(*x,0.))
-        );
+    //let u = u.unwrap();
+    //let v = v.unwrap();
+    let mut diag_view = _s.diag_mut();
+    diag_view.assign(
+        &s.map(|x|Complex32::new(*x,0.))
+    );
 
-        // if u in 1x1
-        let denoised_matrix = if u.len() == 1 {
-            _s.dot(&v)
-        }else {
-            u.dot(&_s).dot(&v)
-        };
+    // if u in 1x1
+    let denoised_matrix = if u.len() == 1 {
+        _s.dot(&v)
+    }else {
+        u.dot(&_s).dot(&v)
+    };
 
-        //let denoised_matrix = u.dot(&_s).dot(&v);
-        matrix.assign(&denoised_matrix);
+    //let denoised_matrix = u.dot(&_s).dot(&v);
+    matrix.assign(&denoised_matrix);
 
-        let final_energy = matrix.iter().map(|x|x.norm_sqr()).sum::<f32>();
+    let final_energy = matrix.iter().map(|x|x.norm_sqr()).sum::<f32>();
 
-        //let energy_makeup = (init_energy / final_energy).sqrt();
-        //matrix.iter_mut().for_each(|x| *x *= energy_makeup);
+    //let energy_makeup = (init_energy / final_energy).sqrt();
+    //matrix.iter_mut().for_each(|x| *x *= energy_makeup);
 
-        DenoiseInfo {
-            singular_values: singular_values,
-            estimated_rank: rank,
-            estimated_noise: noise,
-            init_energy,
-            final_energy,
-        }
+    DenoiseInfo {
+        singular_values: singular_values,
+        estimated_rank: rank,
+        estimated_noise: noise,
+        init_energy,
+        final_energy,
+    }
 }
 
 /// returns the index of the singular value to threshold, along with the estimated matrix variance
@@ -124,7 +126,7 @@ fn cumsum(x:&[f32]) -> Vec<f32> {
     x
 }
 
-fn extract_data(idx:&[usize],readers:&[CflReader]) -> Array2<Complex32> {
+pub fn extract_data(idx:&[usize],readers:&[CflReader]) -> Array2<Complex32> {
     let mut result = Array2::<Complex32>::zeros((idx.len(),readers.len()).f());
     result.axis_iter_mut(Axis(1)).into_par_iter().zip(readers.par_iter()).for_each(|(mut col,reader)|{
         reader.read_into(&idx, col.as_slice_memory_order_mut().unwrap()).unwrap();
@@ -132,8 +134,128 @@ fn extract_data(idx:&[usize],readers:&[CflReader]) -> Array2<Complex32> {
     result
 }
 
-fn insert_data(idx:&[usize], writers:&mut [CflWriter], matrix:&Array2<Complex32>) {
+pub fn insert_data(idx:&[usize], writers:&mut [CflWriter], matrix:&Array2<Complex32>) {
     writers.par_iter_mut().zip(matrix.axis_iter(Axis(1)).into_par_iter()).for_each(|(w,col)|{
         w.write_from(idx, col.as_slice_memory_order().unwrap()).unwrap();
     });
+}
+
+/// find the low-rank approximation of a casorati matrix based on known noise levels
+/// singular values are sorted greatest to least
+fn mp_find_rank(singular_values:&[f32],sigma_sqr:f32,m:usize,n:usize) -> usize {
+    assert!(m >= n,"casorati matrix must have at least as many rows as columns");
+    let mut rank:usize = n;
+    let mut cumulative_sum = 0.;
+    for (i,val) in singular_values.iter().rev().enumerate() {
+        let scale = (m - n + i - 1) as f32 / n as f32;
+        let lamb = val.powi(2) / n as f32;
+        cumulative_sum += lamb;
+        let cumulative_mean = cumulative_sum / (i + 1) as f32;
+        let var = cumulative_mean / scale;
+        if var < sigma_sqr {
+            rank -= 1;
+        }
+    }
+    rank
+}
+
+#[test]
+fn test_find_rank() {
+    let singular_values:Vec<f32> = vec![267.922179,77.677573,77.367026,76.901532,76.218652,75.661715,74.796348,74.245850,73.357911,73.255173,72.823380,72.246012,71.902821,71.266427,70.512379,70.128508,69.673704,69.044314,68.675660,68.103519,67.717366,67.562810,66.985499,66.324698,66.120651,65.819462,65.727307,65.209190,64.870015,64.603728,64.274472,63.772005,63.499366,62.813258,62.612546,62.395864,61.785515,61.286978,60.677350,60.473087,60.263632,60.187295,59.388814,59.090708,58.686399,58.106206,57.738611,57.491265,57.080259,56.797701,56.341278,55.811282,55.283017,55.040263,53.932982,53.506302,53.326709,52.902144,52.633008,51.825234,51.082119,50.702511,50.321345,49.923459,49.815425,48.324251,47.976007];
+    //let rank = find_rank(&singular_values, 4., 1000, 67);
+    let (rank,noise) = mp_estimate_rank(&singular_values, 1000, 67);
+    println!("rank = {}",rank);
+    println!("noise = {}",noise);
+}
+
+// estimates the rank and variance simultaneousely
+fn mp_estimate_rank(singular_values:&[f32],m:usize,n:usize) -> (usize,f32) {
+    assert!(m >= n,"casorati matrix must have at least as many rows as columns");
+    let mut rank:usize = n;
+    let mut cumulative_sum = 0.;
+    let mut noise_estimate = 0.;
+    for (i,val) in singular_values.iter().rev().enumerate() {
+        let scale = (m - n + i - 1) as f32 / n as f32;
+        let lamb = val.powi(2) / n as f32;
+        let lamb_last = singular_values.last().unwrap().powi(2) / n as f32;
+        cumulative_sum += lamb;
+        let cumulative_mean = cumulative_sum / (i + 1) as f32;
+        let var = cumulative_mean / scale;
+        let gamma = (m - ((n-1) - i)) as f32 / n as f32;
+        let range_mp = 4. * gamma.sqrt();
+        let range_data = lamb - lamb_last;
+        let sig_estimate = range_data / range_mp;
+        if sig_estimate < var {
+            rank -= 1;
+            noise_estimate = var;
+        }
+    }
+    (rank,noise_estimate)
+}
+
+
+
+struct PCAInfo {
+    init_energy:f32,
+    final_energy:f32,
+    rank:usize,
+    variance:f32
+}
+
+fn mp_denoise_matrix(matrix:&mut Array2<Complex32>,variance:Option<f32>) -> PCAInfo {
+
+    // m is the number of features (rows)
+    let m = matrix.shape()[0];
+    // n is the number observations (columns)
+    let n = matrix.shape()[1];
+
+    // get the initial energy of the matrix prior to noise reduction
+    let init_energy:f32 = matrix.par_iter().map(|x| x.norm_sqr()).sum();
+
+    // calculate SVD
+    let (u,mut s,v) = matrix.svddc(ndarray_linalg::UVTFlag::Some).unwrap();
+    let u = u.unwrap();
+    let v = v.unwrap();
+
+    // retrieve mutable reference to the singular values
+    let singular_values = s.as_slice_mut().unwrap();
+
+    // determine the rank and noise (if applicable) of the matrix
+    let (rank,var) = match &variance {
+        Some(var) => {
+            let rank = mp_find_rank(singular_values, *var, m, n);
+            (rank,*var)
+        }
+        None => {
+            let (rank,var_estimate) = mp_estimate_rank(singular_values, m, n);
+            (rank,var_estimate)
+        }
+    };
+
+    // do hard thresholding by nullifying singular values above estimated rank
+    singular_values[rank..].fill(0.);
+
+    // reconstruct full diagonal matrix from nullified singular values
+    let s = Array2::from_diag(&s.map(|&x|Complex32::new(x, 0.)));
+
+    // reconstruct data matrix and assign values to original
+    let recon = u.dot(&s).dot(&v);
+    matrix.assign(&recon);
+
+    // find the resulting energy of the denoised matrix
+    let final_energy:f32 = matrix.par_iter().map(|x| x.norm_sqr()).sum();
+
+    // record meta data and return
+    PCAInfo {
+        init_energy,
+        final_energy,
+        rank,
+        variance: var,
+    }
+
+}
+
+#[test]
+fn test_denoise_matrix() {
+    
 }
